a996ae5314e4d04eff6a1b61485469335dc68d92
davidPies <2014119@edtools.psd401.net>
2017-06-08 11:30:50 -0700

    Create Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 package pack1;
 
 public class Main {
-    public static main(String[] args){
+    public static void main(String[] args){
+        method1Start();
+    }
+    public void method1Start(){
+    
     }
 }
