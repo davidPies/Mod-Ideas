2e701cb0d7ffa34c44f7eca6567079aa043ac6d5
davidPies <2014119@edtools.psd401.net>
2017-06-08 11:34:05 -0700

    Create Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 package pack1;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class Main {
     public static void main(String[] args){
-        method1Start();
+        frame1();
     }
-    public void method1Start(){
-    
+    public void frame1(){
+        JFrame myFrame = new JFrame("Sample Frame");
+        myFrame.setSize(300,400);
+        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
     }
 }
