b4221456a638e6717d24b36e3f612fb28f170ea3
davidPies <2014119@edtools.psd401.net>
2018-03-29 11:58:50 -0700

    Update mlgdankcraft.java

--- mlgdankcraft/mlgdankcraft.java
+++ mlgdankcraft/mlgdankcraft.java
@@ -2,7 +2,8 @@ package mlgdankcraft;
 
 @Mod(modid = mlgdankcraft.modid, version = mlgdankcraft.version, name = mlgdankcraft.name)
 public class mlgdankcraft {
-    public static String modid = "mlgdankcraft";
-    public static String version = "1.0.0";
-    public static String name = "MLG Dankcrafts";
+    public static final String modid = "mlgdankcraft";
+    public static final String version = "1.0.0";
+    public static final String name = "MLG Dankcrafts";
+    
 }
