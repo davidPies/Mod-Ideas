38ad1bdae94c9640a098f5a90d7af5958cdb87b1
davidPies <2014119@edtools.psd401.net>
2018-03-30 11:23:52 -0700

    Update satcraft.java

--- satcraft/satcraft.java
+++ satcraft/satcraft.java
@@ -1,5 +1,7 @@
 package satcraft;
 
+
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
@@ -13,8 +15,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = satcraft.satcraft.MODID, version = satcraft.VERSION, name = satcraft.NAME)
-public class trumpmod
+@Mod(modid = satcraft.MODID, version = satcraft.VERSION, name = satcraft.NAME)
+public class satcraft
 {
     public static final String MODID = "satcraft";
     public static final String VERSION = "1.0";
